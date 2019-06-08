3d77c51318d5c230fbad9a2a52f37e59c1b58230
root <root@dev1.dyssevej.cloud>
2019-06-09 01:04:23 +0200

    Added simple design

--- main.go
+++ main.go
@@ -9,22 +9,72 @@ type authenticationForm struct {
     Password string `form:"password"`
 }
 
+type registrationForm struct {
+    Identity string `form:"identity"`
+    Email string `form:"email"`
+    Password string `form:"password"`
+    PasswordRetyped string `form:"password_retyped"`
+}
+
+type recoverForm struct {
+    Identity string `form:"identity"`
+    Password string `form:"password"`
+}
+
+
 func main() {
     r := gin.Default()
 
+    r.Static("/public", "public")
+    
     r.LoadHTMLGlob("views/*")
-    r.GET("/", indexHandler)
-    r.POST("/authenticate", authenticationHandler)
+
+    r.GET("/", getAuthenticationHandler)
+    r.GET("/authenticate", getAuthenticationHandler)
+    r.POST("/authenticate", postAuthenticationHandler)
+
+    r.GET("/register", getRegisterHandler)
+    r.POST("/register", postRegistrationHandler)
+
+    r.GET("/recover", getRecoverHandler)
+    r.POST("/recover", postRecoverHandler)
 
     r.Run(":8080")
 }
 
-func indexHandler(c *gin.Context) {
-    c.HTML(200, "authentication.html", nil)
+func getAuthenticationHandler(c *gin.Context) {
+    c.HTML(200, "authenticate.html", nil)
 }
 
-func authenticationHandler(c *gin.Context) {
+func getRegisterHandler(c *gin.Context) {
+    c.HTML(200, "register.html", nil)
+}
+
+func getRecoverHandler(c *gin.Context) {
+    c.HTML(200, "recover.html", nil)
+}
+
+func postAuthenticationHandler(c *gin.Context) {
     var form authenticationForm
     c.Bind(&form)
-    c.JSON(200, gin.H{"id": form.Identity, "password" : form.Password})
+    c.JSON(200, gin.H{
+        "id": form.Identity,
+        "password" : form.Password })
+}
+
+func postRegistrationHandler(c *gin.Context) {
+    var form registrationForm
+    c.Bind(&form)
+    c.JSON(200, gin.H{
+        "id": form.Identity, 
+        "email": form.Email, 
+        "password" : form.Password, 
+        "password_retyped" : form.PasswordRetyped })
+}
+
+func postRecoverHandler(c *gin.Context) {
+    var form recoverForm
+    c.Bind(&form)
+    c.JSON(200, gin.H{
+        "id": form.Identity })
 }
