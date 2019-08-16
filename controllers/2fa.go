2ac7588ecda61bfec9fa3812768bff176dd89a10
Marc <wraix@users.noreply.github.com>
2019-08-16 10:11:03 +0200

    Simple 2factor generation, missing DB and login validation

---
+++ controllers/2fa.go
@@ -0,0 +1,172 @@
+package controllers
+
+import (
+  "net/http"
+  "bytes"
+  "image/png"
+  "encoding/base64"
+
+  "github.com/sirupsen/logrus"
+  "github.com/gin-gonic/gin"
+  "github.com/gin-contrib/sessions"
+  "github.com/gorilla/csrf"
+  "golang.org/x/oauth2"
+  oidc "github.com/coreos/go-oidc"
+  "github.com/pquerna/otp/totp"
+  "golang-idp-fe/config"
+  "golang-idp-fe/environment"
+  "golang-idp-fe/gateway/idpapi"
+)
+
+type totpForm struct {
+  Passcode string `form:"passcode"`
+  Secret string `form:"secret"`
+}
+
+func Show2Fa(env *environment.State, route environment.Route) gin.HandlerFunc {
+  fn := func(c *gin.Context) {
+
+    log := c.MustGet(environment.LogKey).(*logrus.Entry)
+    log = log.WithFields(logrus.Fields{
+      "func": "Show2Fa",
+    })
+
+    // NOTE: Maybe session is not a good way to do this.
+    // 1. The user access /me with a browser and the access token / id token is stored in a session as we cannot make the browser redirect with Authentication: Bearer <token>
+    // 2. The user is using something that supplies the access token and id token directly in the headers. (aka. no need for the session)
+    var idToken *oidc.IDToken
+    session := sessions.Default(c)
+    idToken = session.Get(environment.SessionIdTokenKey).(*oidc.IDToken)
+    if idToken == nil {
+      c.HTML(http.StatusNotFound, "2fa.html", gin.H{"error": "Identity not found"})
+      c.Abort()
+      return
+    }
+
+    var accessToken *oauth2.Token
+    accessToken = session.Get(environment.SessionTokenKey).(*oauth2.Token)
+    idpapiClient := idpapi.NewIdpApiClientWithUserAccessToken(env.HydraConfig, accessToken)
+
+    // Look up profile information for user.
+    request := idpapi.IdentityRequest{
+      Id: idToken.Subject,
+    }
+    profile, err := idpapi.FetchProfile(config.GetString("idpapi.public.url") + config.GetString("idpapi.public.endpoints.identities"), idpapiClient, request)
+    if err != nil {
+      c.HTML(http.StatusNotFound, "2fa.html", gin.H{"error": "Identity not found"})
+      c.Abort()
+      return
+    }
+
+    totpOpts := totp.GenerateOpts{
+      Issuer: config.GetString("idpui.public.url"),
+      AccountName: profile.Id,
+    }
+    key, err := totp.Generate(totpOpts)
+    if err != nil {
+      log.WithFields(logrus.Fields{
+        "totp.issuer": totpOpts.Issuer,
+        "totp.accountname": totpOpts.AccountName,
+      }).Debug(err)
+      c.HTML(http.StatusInternalServerError, "2fa.html", gin.H{"error": "Failed to generate TOTP code"})
+      c.Abort()
+      return
+    }
+
+    // Convert TOTP key into a PNG
+  	var buf bytes.Buffer
+  	img, err := key.Image(200, 200)
+  	if err != nil {
+      log.Debug(err)
+      c.HTML(http.StatusInternalServerError, "2fa.html", gin.H{"error": "Failed to generate QR code PNG"})
+      c.Abort()
+      return
+  	}
+  	png.Encode(&buf, img)
+    embedQrCode := base64.StdEncoding.EncodeToString(buf.Bytes())
+
+    c.HTML(http.StatusOK, "2fa.html", gin.H{
+      csrf.TemplateTag: csrf.TemplateField(c.Request),
+      "user": idToken.Subject,
+      "name": profile.Name,
+      "email": profile.Email,
+      "issuer": key.Issuer(),
+      "secret": key.Secret(),
+      "qrcode": embedQrCode,
+    })
+  }
+  return gin.HandlerFunc(fn)
+}
+func Submit2Fa(env *environment.State, route environment.Route) gin.HandlerFunc {
+  fn := func(c *gin.Context) {
+
+    log := c.MustGet(environment.LogKey).(*logrus.Entry)
+    log = log.WithFields(logrus.Fields{
+      "func": "Submit2Fa",
+    })
+
+    var form totpForm
+    err := c.Bind(&form)
+    if err != nil {
+      // Do better error handling in the application.
+      c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+      c.Abort()
+      return
+    }
+
+    session := sessions.Default(c)
+
+    // NOTE: Maybe session is not a good way to do this.
+    // 1. The user access /me with a browser and the access token / id token is stored in a session as we cannot make the browser redirect with Authentication: Bearer <token>
+    // 2. The user is using something that supplies the access token and id token directly in the headers. (aka. no need for the session)
+    var idToken *oidc.IDToken
+    idToken = session.Get(environment.SessionIdTokenKey).(*oidc.IDToken)
+    if idToken == nil {
+      c.HTML(http.StatusNotFound, "2fa.html", gin.H{"error": "Identity not found"})
+      c.Abort()
+      return
+    }
+
+    var accessToken *oauth2.Token
+    accessToken = session.Get(environment.SessionTokenKey).(*oauth2.Token)
+    idpapiClient := idpapi.NewIdpApiClientWithUserAccessToken(env.HydraConfig, accessToken)
+
+    // Look up profile information for user.
+    request := idpapi.IdentityRequest{
+      Id: idToken.Subject,
+    }
+    profile, err := idpapi.FetchProfile(config.GetString("idpapi.public.url") + config.GetString("idpapi.public.endpoints.identities"), idpapiClient, request)
+    if err != nil {
+      c.HTML(http.StatusNotFound, "2fa.html", gin.H{"error": "Identity not found"})
+      c.Abort()
+      return
+    }
+
+    // We need to validate that the user entered a correct passcode form the authenticator app before enabling 2fa on the profile. Or we risk locking the user out of the system.
+    // We should also generate a set of one time recovery codes and display to the user (simple generate a set of random codes and let the user print them)
+    // see https://github.com/pquerna/otp, https://help.github.com/en/articles/configuring-two-factor-authentication-recovery-methods
+  	valid := totp.Validate(form.Passcode, form.Secret)
+    if valid == true {
+      log.WithFields(logrus.Fields{
+        "id": profile.Id,
+        /* DO NOT LOG THIS IS LIKE PASSWORDS
+        "passcode": form.Passcode,
+        "secret": form.Secret,
+        */
+      }).Debug("Passcode verified")
+
+      // Add secret to Identity in db and enable setting to require it. Remeber to store the secret using bcrypt hashing?
+      log.WithFields(logrus.Fields{"id": profile.Id}).Debug("Enable 2fa for Identity")
+
+      log.WithFields(logrus.Fields{"fixme":1}).Debug("Redirect to where we came from")
+      log.WithFields(logrus.Fields{"redirect_to": "/me"}).Debug("Redirecting")
+      c.Redirect(http.StatusFound, "/me")
+      return
+    }
+
+    // Deny by default. Failed to fill in the form correctly.
+    c.Redirect(http.StatusFound, route.URL)
+    c.Abort()
+  }
+  return gin.HandlerFunc(fn)
+}
