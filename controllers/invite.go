75f6316c5b6ae6ce906c8b781d3582db7fb37366
Marc Frisenette <marc@cybertron.dk>
2019-09-16 11:44:07 +0200

    WIP

---
+++ controllers/invite.go
@@ -0,0 +1,147 @@
+package controllers
+
+import (
+  "net/http"
+  "github.com/sirupsen/logrus"
+  "github.com/gin-gonic/gin"
+  "github.com/gorilla/csrf"
+  "github.com/gin-contrib/sessions"
+  "golang.org/x/oauth2"
+  oidc "github.com/coreos/go-oidc"
+  idp "github.com/charmixer/idp/client"
+
+  "github.com/charmixer/idpui/config"
+  "github.com/charmixer/idpui/environment"
+)
+
+type inviteForm struct {
+  Email string `form:"email" binding:"required"`
+  Username string `form:"username"`
+  GrantedScopes []string `form:"granted_scopes[]"`
+  FollowIdentities []string `form:"follow_identities[]"`
+}
+
+func ShowInvite(env *environment.State, route environment.Route) gin.HandlerFunc {
+  fn := func(c *gin.Context) {
+
+    log := c.MustGet(environment.LogKey).(*logrus.Entry)
+    log = log.WithFields(logrus.Fields{
+      "func": "ShowInvite",
+    })
+
+    // NOTE: Maybe session is not a good way to do this.
+    // 1. The user access /me with a browser and the access token / id token is stored in a session as we cannot make the browser redirect with Authentication: Bearer <token>
+    // 2. The user is using something that supplies the access token and id token directly in the headers. (aka. no need for the session)
+    var idToken *oidc.IDToken
+    session := sessions.Default(c)
+    idToken = session.Get(environment.SessionIdTokenKey).(*oidc.IDToken)
+    if idToken == nil {
+      c.HTML(http.StatusNotFound, "invite.html", gin.H{"error": "Identity not found"})
+      c.Abort()
+      return
+    }
+
+    var accessToken *oauth2.Token
+    accessToken = session.Get(environment.SessionTokenKey).(*oauth2.Token)
+    idpClient := idp.NewIdpClientWithUserAccessToken(env.HydraConfig, accessToken)
+
+    // Sanity check. Access token id must exist.
+    identityRequest := &idp.IdentitiesReadRequest{
+      Id: idToken.Subject,
+    }
+    identity, err := idp.ReadIdentity(idpClient, config.GetString("idp.public.url") + config.GetString("idp.public.endpoints.identities"), identityRequest)
+    if err != nil {
+      c.HTML(http.StatusNotFound, "invite.html", gin.H{"error": "Identity not found"})
+      c.Abort()
+      return
+    }
+
+    log.WithFields(logrus.Fields{"fixme": 1}).Debug("Find all permission identity is allowed to grant (maygrant?)")
+
+    s := []string{"read:identity", "openid"}
+    f := []string{identity.Id}
+
+    var mayGrantScopes = make(map[int]map[string]string)
+    for index, name := range s {
+      mayGrantScopes[index] = map[string]string{
+        "name": name,
+      }
+    }
+
+    var followIdentities = make(map[int]map[string]string)
+    for index, name := range f {
+      followIdentities[index] = map[string]string{
+        "name": name,
+      }
+    }
+
+    c.HTML(http.StatusOK, "invite.html", gin.H{
+      /*"links": []map[string]string{
+        {"href": "/public/css/dashboard.css"},
+      },*/
+      "__links": []map[string]string{
+        {"href": "/public/css/main.css"},
+      },
+      "__title": "Invite",
+      csrf.TemplateTag: csrf.TemplateField(c.Request),
+      "name": identity.Name,
+      "mayGrantScopes": mayGrantScopes,
+      "followIdentities": followIdentities,
+    })
+  }
+  return gin.HandlerFunc(fn)
+}
+
+func SubmitInvite(env *environment.State, route environment.Route) gin.HandlerFunc {
+  fn := func(c *gin.Context) {
+
+    log := c.MustGet(environment.LogKey).(*logrus.Entry)
+    log = log.WithFields(logrus.Fields{
+      "func": "SubmitInvite",
+    })
+
+    var form inviteForm
+    err := c.Bind(&form)
+    if err != nil {
+      c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+      c.Abort()
+      return
+    }
+
+    // NOTE: Maybe session is not a good way to do this.
+    // 1. The user access /me with a browser and the access token / id token is stored in a session as we cannot make the browser redirect with Authentication: Bearer <token>
+    // 2. The user is using something that supplies the access token and id token directly in the headers. (aka. no need for the session)
+    var idToken *oidc.IDToken
+    session := sessions.Default(c)
+    idToken = session.Get(environment.SessionIdTokenKey).(*oidc.IDToken)
+    if idToken == nil {
+      c.HTML(http.StatusNotFound, "invite.html", gin.H{"error": "Identity not found"})
+      c.Abort()
+      return
+    }
+
+    var accessToken *oauth2.Token
+    accessToken = session.Get(environment.SessionTokenKey).(*oauth2.Token)
+    idpClient := idp.NewIdpClientWithUserAccessToken(env.HydraConfig, accessToken)
+
+    inviteRequest := &idp.IdentitiesInviteCreateRequest{
+      Id: idToken.Subject,
+      Email: form.Email,
+      Username: form.Username,
+      GrantedScopes: form.GrantedScopes,
+      PleaseFollow: []string{idToken.Subject},
+      TTL: 60*60*24, // 1 hour
+    }
+    invite, err := idp.CreateInvite(idpClient, config.GetString("idp.public.url") + config.GetString("idp.public.endpoints.invite"), inviteRequest)
+    if err != nil {
+      log.WithFields(logrus.Fields{"email": inviteRequest.Email, "username": inviteRequest.Username}).Debug("Failed to create invite")
+      c.HTML(http.StatusInternalServerError, "invite.html", gin.H{"error": err.Error()})
+      c.Abort()
+      return
+    }
+
+    log.WithFields(logrus.Fields{"id":invite.Id}).Debug("Invite created")
+    c.HTML(http.StatusOK, "invite.html", gin.H{"id": invite.Id})
+  }
+  return gin.HandlerFunc(fn)
+}
---
+++ controllers/inviteregister.go
@@ -0,0 +1,199 @@
+package controllers
+
+import (
+  "strings"
+  "net/http"
+  "github.com/sirupsen/logrus"
+  "github.com/gin-gonic/gin"
+  "github.com/gorilla/csrf"
+  "github.com/gin-contrib/sessions"
+  idp "github.com/charmixer/idp/client"
+
+  "github.com/charmixer/idpui/config"
+  "github.com/charmixer/idpui/environment"
+)
+
+type inviteRegisterForm struct {
+    Username string `form:"username" binding:"required"`
+    Name string `form:"display-name" binding:"required"`
+    Email string `form:"email" binding:"required"`
+    Password string `form:"password" binding:"required"`
+    PasswordRetyped string `form:"password_retyped" binding:"required"`
+}
+
+func ShowInviteRegister(env *environment.State, route environment.Route) gin.HandlerFunc {
+  fn := func(c *gin.Context) {
+
+    log := c.MustGet(environment.LogKey).(*logrus.Entry)
+    log = log.WithFields(logrus.Fields{
+      "func": "ShowInviteRegister",
+    })
+
+    session := sessions.Default(c)
+
+    // Retain the values that was submittet, except passwords ?!
+    username := session.Get("register.username")
+    displayName := session.Get("register.display-name")
+    email := session.Get("register.email")
+
+    errors := session.Flashes("register.errors")
+    err := session.Save() // Remove flashes read, and save submit fields
+    if err != nil {
+      log.Debug(err.Error())
+    }
+
+    var errorUsername string
+    var errorPassword string
+    var errorPasswordRetyped string
+    var errorEmail string
+    var errorDisplayName string
+
+    if len(errors) > 0 {
+      errorsMap := errors[0].(map[string][]string)
+      for k, v := range errorsMap {
+        if k == "errorUsername" && len(v) > 0 {
+          errorUsername = strings.Join(v, ", ")
+        }
+
+        if k == "errorPassword" && len(v) > 0 {
+          errorPassword = strings.Join(v, ", ")
+        }
+
+        if k == "errorPasswordRetyped" && len(v) > 0 {
+          errorPasswordRetyped = strings.Join(v, ", ")
+        }
+
+        if k == "errorEmail" && len(v) > 0 {
+          errorEmail = strings.Join(v, ", ")
+        }
+
+        if k == "errorDisplayName" && len(v) > 0 {
+          errorDisplayName = strings.Join(v, ", ")
+        }
+      }
+    }
+
+    c.HTML(200, "register.html", gin.H{
+      "__links": []map[string]string{
+        {"href": "/public/css/main.css"},
+      },
+      "__title": "Register",
+      csrf.TemplateTag: csrf.TemplateField(c.Request),
+      "username": username,
+      "displayName": displayName,
+      "email": email,
+      "errorUsername": errorUsername,
+      "errorPassword": errorPassword,
+      "errorPasswordRetyped": errorPasswordRetyped,
+      "errorEmail": errorEmail,
+      "errorDisplayName": errorDisplayName,
+    })
+  }
+  return gin.HandlerFunc(fn)
+}
+
+func SubmitInviteRegister(env *environment.State, route environment.Route) gin.HandlerFunc {
+  fn := func(c *gin.Context) {
+
+    log := c.MustGet(environment.LogKey).(*logrus.Entry)
+    log = log.WithFields(logrus.Fields{
+      "func": "SubmitInviteRegister",
+    })
+
+    var form inviteRegisterForm
+    err := c.Bind(&form)
+    if err != nil {
+      // Do better error handling in the application.
+      c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+      c.Abort()
+      return
+    }
+
+    session := sessions.Default(c)
+
+    // Save values if submit fails
+    session.Set("register.username", form.Username)
+    session.Set("register.display-name", form.Name)
+    session.Set("register.email", form.Email)
+    err = session.Save()
+    if err != nil {
+      log.Debug(err.Error())
+    }
+
+    errors := make(map[string][]string)
+
+    username := strings.TrimSpace(form.Username)
+    if username == "" {
+      errors["errorUsername"] = append(errors["errorUsername"], "Missing username")
+    }
+
+    password := form.Password
+    if strings.TrimSpace(password) == "" {
+      errors["errorPassword"] = append(errors["errorPassword"], "Missing password. Hint: Not allowed to be all whitespace")
+    }
+
+    retypedPassword := form.PasswordRetyped
+    if retypedPassword != password {
+      errors["errorPasswordRetyped"] = append(errors["errorPasswordRetyped"], "Must match password")
+    }
+
+    if len(errors) > 0 {
+      session.AddFlash(errors, "register.errors")
+      err = session.Save()
+      if err != nil {
+        log.Debug(err.Error())
+      }
+      log.WithFields(logrus.Fields{"redirect_to": route.URL}).Debug("Redirecting")
+      c.Redirect(http.StatusFound, route.URL)
+      c.Abort();
+      return
+    }
+
+    if password == retypedPassword { // Just for safety is caught in the input error detection.
+
+      idpClient := idp.NewIdpClient(env.IdpApiConfig)
+
+      identityRequest := &idp.IdentitiesCreateRequest{
+        Subject: form.Username,
+        Email: form.Email,
+        Password: form.Password,
+        Name: form.Name,
+      }
+      identity, err := idp.CreateIdentity(idpClient, config.GetString("idp.public.url") + config.GetString("idp.public.endpoints.identities"), identityRequest)
+      if err != nil {
+        log.Debug(err.Error())
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        c.Abort()
+        return
+      }
+
+      session := sessions.Default(c)
+
+      // Cleanup session
+      session.Delete("register.username")
+      session.Delete("register.display-name")
+      session.Delete("register.email")
+      session.Delete("register.errors")
+
+      // Propagate username to authenticate controller
+      session.Set("authenticate.username", identity.Subject)
+
+      err = session.Save()
+      if err != nil {
+        log.Debug(err.Error())
+      }
+
+      // Registration successful, return to create new ones, but with success message
+      redirectTo := config.GetString("idpui.public.url") + config.GetString("idpui.public.endpoints.profile")
+      log.WithFields(logrus.Fields{"redirect_to": redirectTo}).Debug("Redirecting")
+      c.Redirect(http.StatusFound, redirectTo)
+      c.Abort()
+      return
+    }
+
+    // Deny by default. Failed to fill in the form correctly.
+    c.Redirect(http.StatusFound, route.URL)
+    c.Abort()
+  }
+  return gin.HandlerFunc(fn)
+}
